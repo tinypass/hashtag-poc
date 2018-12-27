d554588fb44c27900bef2d087f4508e541152ff6
maksimtentiuk <maksim.tentyuk@tinypass.com>
2018-12-28 01:50:06 +0400

    Public interface

--- modal.js
+++ modal.js
@@ -1,10 +1,26 @@
 class modal {
-    constructor(type) {
-        this._createModal(type)
+    constructor(type, trackingId) {
+        this._createModal(type, trackingId)
     }
 
-    _createModal(type) {
-        var self = this;
+    static init(pbjsSrc, adUnits, adService, interestsTargeting, segmentsTargeting, urlTargeting, subscribeLink) {
+        this.prototype.adSlotId = 'ppb-ad-1';
+        this.prototype.externalEventId = 'offer-join-now';
+        this.prototype.subscribeLink = subscribeLink;
+        this.loadPB(pbjsSrc, adUnits, adService, interestsTargeting, segmentsTargeting, urlTargeting);
+        this.addCss();
+    }
+
+    static createAdModal(trackingId) {
+        let newModal = new modal('ad', trackingId)
+    }
+
+    static createSubscribeModal(trackingId) {
+        let newModal = new modal('subscribe', trackingId)
+    }
+
+    _createModal(type, trackingId) {
+        let self = this;
         jQuery(function ($) {
             let base = $(`<div class="modal"><div class="modal-content" id="rec-modal">
     <div class="pn-header">
@@ -24,7 +40,7 @@ class modal {
     <div id="li_recommended_content">
     </div>
 </div></div>`);
-            var extra = $('<div></div>');
+            let extra = $('<div></div>');
 
             self._parseLiftIgnite(base);
 
@@ -32,7 +48,22 @@ class modal {
                 base.remove()
             });
 
+
             $('body').append(base);
+
+            let oldSize = $(document).width();
+
+            $(window).resize(function () {
+                let newWidth = $("#rec-modal", base).width();
+                // if we crossed a threshold, call refresh() to get a new ad
+                if (oldSize < 970 && newWidth >= 970) {
+                    oldSize = newWidth;
+                    googletag.pubads().refresh()
+                } else if (oldSize >= 970 && newWidth < 970) {
+                    oldSize = newWidth;
+                    googletag.pubads().refresh()
+                }
+            });
 
             switch (type) {
                 case 'ad':
@@ -44,31 +75,45 @@ class modal {
                 case 'subscribe':
                     extra = $(`<div class="join-now">
                                   <p class="CTA_text">Get your chive on. Join now.</p>
-                                  <button class="pn-modal__join" type="button" id="subscribe" external-event="offer-join-now" data-link="${self.subscribeLink}">BECOME A MEMBER</button>
+                                  <button class="pn-modal__join" type="button" id="subscribe" data-link="${self.subscribeLink}">BECOME A MEMBER</button>
                                   </div>
                                   <div class="pn-modal__footer">
                                   </div>
                                 </div>`);
                     $('.pn-modal__join', extra).click(function () {
+                        if (trackingId) {
+                            try {
+                                tp.log.logMicroConversion(
+                                    trackingId,
+                                    self.externalEventId,
+                                    {}
+                                );
+                            }
+                            catch (e) {
+                                // Suppress
+                            }
+                        }
+
                         location.href = $(this).attr('data-link');
                     });
                     break;
             }
 
-            $('.modal-content',base).append(extra);
+            $('.modal-content', base).append(extra);
         });
     }
 
     _initAdServer() {
-        // if (pbjs_piano.initAdserverSet) return;
+        let self = this;
+        if (pbjs_piano.initAdserverSet) return;
         pbjs_piano.initAdserverSet = true;
         googletag.cmd.push(function () {
             pbjs_piano.que.push(function () {
-                var slots = googletag
+                let slots = googletag
                     .pubads()
                     .getSlots()
                     .filter(function (slot) {
-                        return slot.getSlotElementId() == "ppb-ad-1";
+                        return slot.getSlotElementId() === self.adSlotId;
                     });
                 pbjs_piano.setTargetingForGPTAsync();
                 googletag.pubads().refresh(slots);
@@ -77,10 +122,10 @@ class modal {
     }
 
     _createAds(base) {
-        var self = this;
+        let self = this;
         jQuery(function ($) {
             // place our ad after the given selector
-            let div = $('<div id="ppb-ad-1"></div>');
+            let div = $(`<div id="${self.adSlotId}"></div>`);
             $('#ad-container', base).append(div);
 
             // configureable timeout
@@ -108,18 +153,18 @@ class modal {
 
     _createArticleCard(base, article) {
         jQuery(function ($) {
-            var card = $(`<div class="card"><a href="${article.url}"> <img class="img" src="${article.image}" alt="Avatar" style="width:100%">
+            let card = $(`<div class="card"><a href="${article.url}"> <img class="img" src="${article.image}" alt="Avatar" style="width:100%">
                                 <h4><b>${article.title}</b></h4></a></div>`);
             $('#li_recommended_content', base).append(card)
         });
     }
 
     _parseLiftIgnite(base) {
-        var self = this;
-        var li_num_posts = 0;
-        var li_tag = document.getElementById('liftigniter-received-json');
-        var li_timer = setInterval(function () {
-            var li_json = JSON.parse(li_tag.innerText);
+        let self = this;
+        let li_num_posts = 0;
+        let li_tag = document.getElementById('liftigniter-received-json');
+        let li_timer = setInterval(function () {
+            let li_json = JSON.parse(li_tag.innerText);
             if (li_num_posts !== li_json.length) {
                 li_json.forEach((article) => {
                     self._createArticleCard(base, article)
@@ -131,7 +176,8 @@ class modal {
     }
 
     static loadPB(pbjsSrc, adUnits, adService, interestsTargeting, segmentsTargeting, urlTargeting) {
-        var s = document.createElement("script");
+        let self = this;
+        let s = document.createElement("script");
         s.src = pbjsSrc;
         s.type = "text/javascript";
         s.async = true;
@@ -153,8 +199,8 @@ class modal {
 
         googletag.cmd.push(function () {
 
-            var mapping = googletag.sizeMapping().addSize([1290, 0], [970, 250]).addSize([0, 0], [300, 250]).build();
-            googletag.defineSlot(adService, mapping, 'ppb-ad-1').defineSizeMapping(mapping).addService(googletag.pubads())
+            let mapping = googletag.sizeMapping().addSize([1290, 0], [970, 250]).addSize([0, 0], [300, 250]).build();
+            googletag.defineSlot(adService, mapping, self.prototype.adSlotId).defineSizeMapping(mapping).addService(googletag.pubads())
             googletag.pubads().setCentering(true);
             googletag.pubads().collapseEmptyDivs();
             googletag.pubads().enableSingleRequest();
@@ -168,14 +214,9 @@ class modal {
     }
 
     static addCss() {
-        var styleNode = document.createElement("style");
+        let styleNode = document.createElement("style");
         styleNode.innerText = `.modal {display: block;position: fixed;z-index: 20001;left: 0;top: 0;width: 100%;height: 100%;overflow: auto;background-color: rgb(0,0,0);background-color: rgba(0,0,0,0.4);}.modal-content {background-color: #fefefe;margin: 0 auto;border: 1px solid #888;padding-bottom: 20px;width: 75%;}/*font*/@font-face {font-family: 'Helvetica Neue';font-weight: bold;font-style: normal;}@font-face {font-family: 'Helvetica Neue';font-weight: 300;font-style: normal;}body{background-color: #ffffff;color: #000000;}.content{width: 970px;margin: 15px auto 10px auto;display: block;text-align: center;}.visually-hidden {position: absolute;width: 1px;height: 1px;margin: -1px;border: 0;padding: 0;white-space: nowrap;-webkit-clip-path: inset(100%);clip-path: inset(100%);clip: rect(0 0 0 0);overflow: hidden;}.pn-modal__close {float: right;margin-top: 15px;margin-right: 25px;width: 18px;height: 18px;}.pn-modal__close svg {fill: #00C300;-webkit-transition: fill 0.2s linear;transition: fill 0.2s linear;}.pn-modal__close:hover svg,.pn-modal__close:focus svg {fill: #00C300;}h1 {font-size: 25px;font-weight: bold;line-height: 38px;font-family: 'Helvetica Neue', sans-serif;margin: 0;}.pn-header {width: 100%;height: 77px;background-color: #000000;border-bottom: 3px solid #00C300;}.pn-header__background {width: 173px;height: 36.18px;float: left;margin: 20px 35px;}.unbutton {color: inherit;text-decoration: none;background-color: rgba(0, 0, 0, 0);-webkit-box-shadow: none;box-shadow: none;text-shadow: none;border: 0;}#li_recommended_content{width: 100%;display: flex;flex-wrap: wrap;flex-direction: row;justify-content: center;margin-bottom: 20px;}.card {transition: 0.3s;width: 20%;position: relative;margin-right: 25px;margin-left: 25px;margin-top: 15px;}.card:hover{box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);}.container{width: 100%;}.card h4 {font-size: 12px;position: absolute;z-index: 20002;bottom: 30px;margin: 0 10px;background-color: #000000;line-height: 20px;color: #ffffff;}a{color: #ffffff;text-decoration: none;font-family: 'Helvetica Neue';font-style: normal;}.img{width: 100%;height: 100%;border: 1px solid #00C300;}.ad{margin: auto;display: block;padding: 25px;width: 100%;}.ad_text{text-align: center;font-size: 11px;color: #00C300;font-family: 'Helvetica Neue';font-style: normal;}.CTA_text{font-family: 'Helvetica Neue', sans-serif;text-align: center;font-size: 29px;margin: 40px;}.pn-modal__join {width: 250px;margin: 0 auto;display: block;padding: 10px 5px;font-family: 'Helvetica', sans-serif;font-weight: 900;font-size: 16px;line-height: 20px;color: #ffffff;background-color: #00C300;transition: background-color 0.2s linear;}.pn-modal__join:hover,.pn-modal__join:focus {background-color: #00C300;color: #ffffff;border: 2px solid #00c300;}.pn-modal__join:active {opacity: 0.8;}.pn-modal__join:visited{color: #ffffff;background-color: #00c300;}@media only screen and (max-width: 1448px) {.content{width: 480px;}}`;
         document.getElementsByTagName("head")[0].appendChild(styleNode);
     }
 
-    static init(pbjsSrc, adUnits, adService, interestsTargeting, segmentsTargeting, urlTargeting, subscribeLink) {
-        this.loadPB(pbjsSrc, adUnits, adService, interestsTargeting, segmentsTargeting, urlTargeting);
-        this.prototype.subscribeLink = subscribeLink;
-        this.addCss();
-    }
 }
